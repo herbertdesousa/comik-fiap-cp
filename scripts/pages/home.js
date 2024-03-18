d03036c9c2dfa4328de23bd4342d19f3e2311078
herbertdesousa <sousaherbert138@gmail.com>
2024-03-18 12:03:16 -0300

    FEAT: recommended items on home from data.json

--- scripts/pages/home.js
+++ scripts/pages/home.js
@@ -2,21 +2,47 @@ loadData((json) => {
   const { items, home } = json;
 
   //#region main item
-  const item = items[home.main_item_index];
+  const mainItem = items[home.main_item_index];
 
-  if (!item) {
+  if (!mainItem) {
     alert('home.main_item_index não encontrado');
     return;
   }
 
-  if (item.tag) {
-    $('#main-item').append(`<span class="tag">${item.tag}</span>`)
+  if (mainItem.tag) {
+    $('#main-item').append(`<span class="tag">${mainItem.tag}</span>`)
   }
 
-  $('#main-item h2').html(item.title);
+  $('#main-item h2').html(mainItem.title);
   $('#main-item').append(
-    `<img src="${item.image_url}" alt="${item.image_alt}">`,
+    `<img src="${mainItem.image_url}" alt="${mainItem.image_alt}">`,
   );
 
   //#endregion
+
+  //#region carrousel items
+  const carrouselItems = home.carrousel_items_index
+    .map(itemIndex => items[itemIndex])
+    .filter(item => item);
+  
+  carrouselItems.forEach(item => {
+    $('#recommended-items').append(`
+      <li class="list-item list-item-sm">
+        <div class="list-item-image">
+          <img src="${item.image_url}" alt="${item.image_alt}" />
+          
+          ${item.tag && `<span class="tag">${item.tag}</span>`}
+        </div>
+
+        <div class="list-item-bottom">
+          <span>${item.title}</span>
+          
+          <span class="list-item-bottom-likes">${item.trending_info}</span>
+        </div>
+      </li>
+    `)
+  });
+
+  //#endregion
+  
 });
