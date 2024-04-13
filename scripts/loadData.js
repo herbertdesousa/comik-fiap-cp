611d324ae39a50774c1296d920dd88f15189468a
herbertdesousa <sousaherbert138@gmail.com>
2024-04-13 15:56:15 -0300

    FEAT: removed script and json :c

--- scripts/loadData.js
+++
@@ -1,9 +0,0 @@
-const JSON_PATH = window.location.origin + '/data.json';
-
-async function loadData(onLoad) {
-  const response = await fetch(JSON_PATH);
-  
-  const json = await response.json()
-
-  onLoad(json);
-}
